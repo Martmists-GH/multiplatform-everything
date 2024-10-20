404af76039e61383c7c4e6c1bdf586741949243b
Martmists <martmists@gmail.com>
2024-10-20 10:48:13 +0200

    inline fragments
    closes #1

--- src/commonMain/kotlin/com/martmists/multiplatform/graphql/SchemaRequestContext.kt
+++ src/commonMain/kotlin/com/martmists/multiplatform/graphql/SchemaRequestContext.kt
@@ -22,14 +22,20 @@ class SchemaRequestContext(
     }
 
     internal fun expand(type: String, selection: List<Selection>): List<Field> {
-        val fields = selection.filterIsInstance<Field>() + selection.filterIsInstance<FragmentSpread>().flatMap { expand(type, it) }
+        val fields = selection.filterIsInstance<Field>() +
+                selection.filterIsInstance<FragmentSpread>().flatMap { expand(type, it) } +
+                selection.filterIsInstance<InlineFragment>().flatMap { expand(type, it) }
         return fields
     }
 
     private fun expand(type: String, fragment: FragmentSpread): List<Field> {
         val set = frags.find { it.typeCond.name == type && it.name == fragment.name }?.selectionSet ?: return emptyList()
-        val fields = set.filterIsInstance<Field>() + set.filterIsInstance<FragmentSpread>().flatMap { expand(type, it) }
-        return fields
+        return expand(type, set)
+    }
+
+    private fun expand(type: String, fragment: InlineFragment): List<Field> {
+        if (fragment.typeCond?.name != type) return emptyList()
+        return expand(type, fragment.selectionSet)
     }
 
     internal fun <T> variable(name: String): T {
--- src/commonMain/kotlin/com/martmists/multiplatform/graphql/parser/ast/InlineFragment.kt
+++ src/commonMain/kotlin/com/martmists/multiplatform/graphql/parser/ast/InlineFragment.kt
@@ -3,7 +3,7 @@ package com.martmists.multiplatform.graphql.parser.ast
 import com.martmists.multiplatform.validation.lexer.Loc
 
 data class InlineFragment(
-    val typeCond: Type?,
+    val typeCond: NamedType?,
     val directives: List<Directive>,
     val selectionSet: List<Selection>,
     override val loc: Loc
