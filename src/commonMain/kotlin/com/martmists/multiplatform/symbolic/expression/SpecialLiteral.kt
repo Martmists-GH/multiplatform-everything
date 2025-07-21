c0ef5909426fb56817abed8ebe99d0d6cdde5ae7
Martmists <martmists@gmail.com>
2025-07-22 01:04:14 +0200

    small fix for 1.2.7 build

--- src/commonMain/kotlin/com/martmists/multiplatform/symbolic/expression/SpecialLiteral.kt
+++ src/commonMain/kotlin/com/martmists/multiplatform/symbolic/expression/SpecialLiteral.kt
@@ -5,10 +5,10 @@ import com.martmists.multiplatform.symbolic.EvaluationContext
 /**
  * Represents a [Literal] with specific LaTeX expression, such as `\pi`.
  */
-class SpecialLiteral(value: Double, val latex: String) : Literal(value) {
+class SpecialLiteral(value: Double, val latexText: String) : Literal(value) {
     override fun evaluate(context: EvaluationContext) = this
 
-    override fun latex() = latex
+    override fun latex() = latexText
 
-    override fun toString(): String = "SpecialLiteral(value=$value, latex=$latex)"
+    override fun toString(): String = "SpecialLiteral(value=$value, latex=$latexText)"
 }
